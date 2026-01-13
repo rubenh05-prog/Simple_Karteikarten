bea7e334407318d61f43a3342af9abddabdd1459
Robin <rubenh05@googlemail.com>
2026-01-13 09:41:23 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "simple-karteikarten-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c =>
+      c.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./apple-touch-icon.png"
+      ])
+    )
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
